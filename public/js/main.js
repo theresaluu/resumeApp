7a59bda4721be07f10f3d58d7ea76a44b0d952b9
Theresa Luu <theresa.luu@gmail.com>
2013-06-27 15:49:50 -0700

    things working: capturing info for AJAXing for signup.js; things not working: rendering all JSON info from server.'

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,4 @@
-/*$(document).ready(function() {
-	console.log('im alive');
+$(document).ready(function() {
 	$.ajax("/api/resumes/51c20830a3672af4b3000001", {
 
 		complete : function(response){
@@ -8,16 +7,14 @@
 			var fullName = first + " " + last;
 			$('#name').html(fullName);
 
+			//contact info block
 			var email = response.responseJSON.contact_info.email;
 			$('#email').html(email);
-
-			console.log(email);
 
 			var phone = response.responseJSON.contact_info.phone;
 			$('#phone').html(phone);
 
-			console.log(phone);
-
+			console.log(response.responseJSON);
 
 			var street = response.responseJSON.contact_info.street_address.street;
 			var city = response.responseJSON.contact_info.street_address.city;
@@ -27,84 +24,176 @@
 
 			$('#address').html(address);
 
-			console.log(response.responseJSON);
-
+			//skills
 			var categories = new Array;
 			var tricks = new Array;
 			var exppts = new Array;
 			var type = new Array;
 
-
 			for(i=0; i< response.responseJSON.skill.length; i++)
 			{
-
 				categories[i] = response.responseJSON.skill[i].category;
 				tricks[i] = response.responseJSON.skill[i].title;
 				exppts[i] = response.responseJSON.skill[i].experience;
 				type [i] = categories[i] + " (level: " + exppts[i] +") ";
 
-
+				$('dl').append('<dt>' + categories[i] + '</dt>');
+				$('dl').append('<dd>' + tricks[i] + '</dd>');
+				$('dl').append('<p>' + exppts[i] + '</p>');
 			}
-			console.log("categories: " + categories);
-			console.log("tricks: " + tricks);
-			console.log("experience points: " + exppts);
-			
-			$('#tricks1').html(tricks[0]);
-			$('#tricks2').html(tricks[1]);
-			$('#tricks3').html(tricks[2]);
-			$('#tricks4').html(tricks[3]);
-			$('#tricks5').html(tricks[4]);
 
-			$('#type1').html(type[0]);
-			$('#type2').html(type[0]);
-			$('#type3').html(type[0]);
-			$('#type4').html(type[0]);
-			$('#type5').html(type[0]);
+			//accomplishments
+			var acDate = new Array;
+			var acTitle = new Array;
+			var acDes = new Array;
+
 
 			for(i=0; i< response.responseJSON.accomplishments.length; i++)
 			{
+				acDate[i] = response.responseJSON.accomplishments[i].month_year;
+				acTitle[i] = response.responseJSON.accomplishments[i].title;
+				acDes[i] = response.responseJSON.accomplishments[i].description;
 
-				accmpdate[i] = response.responseJSON.accomplishments[i].month_year;
-				accmpdes[i] = response.responseJSON.accomplishments[i].description;
-				accmptitle[i] = response.responseJSON.accomplishments[i].accmptitle;
-				accomplishments[i] = accomptitle[i] + ": " + accmpdate[i];
+				$('#accomplish').append('<dl>' + acDate[i] + '</dl>');
+				$('#accomplish').append('<dl>' + acTitle[i] + '</dl>');
+				$('#accomplish').append('<p>' + acDes[i] + '</p>');
+			}
 
+
+			//work history 'experience'
+			var xp_start_month_year = new Array;
+			var xp_end_month_year = new Array;
+			var xp_role = new Array;
+			var xp_project = new Array;
+			var xp_org = new Array;
+			var xp_location = new Array;
+			var xp_duration = new Array;
+			var xp_responsibilities = [];
+
+			for(i=0; i< response.responseJSON.experience.length; i++)
+			{
+				xp_start_month_year[i] = response.responseJSON.experience[i].start_month_year;
+				xp_end_month_year[i] = response.responseJSON.experience[i].end_month_year;
+				xp_role[i] = response.responseJSON.experience[i].role;
+				xp_project[i] = response.responseJSON.experience[i].project;
+				xp_org[i] = response.responseJSON.experience[i].organization;
+				xp_location[i] = response.responseJSON.experience[i].location;
+				xp_duration[i] = xp_start_month_year[i] + '-' + xp_end_month_year[i];
+				
+				$.each(response.responseJSON.experience[i].responsibilities, function(index, item) {
+					$('.id').append('<p>' + index + '</p>');
+				});
+
+				$('.xp').append('<li>' + xp_duration[i] + '</li>');
+				$('.xp').append('<li>' + xp_role[i] + '</li>');
+				$('.xp').append('<li>' + xp_org[i] + '</li>');
+				$('.xp').append('<li>' + xp_location[i] + '</li>');
+				$('.xp').append('<li>' + xp_responsibilities[i] + '</li>');
+				$('.xp').append('<li>' + xp_org[i] + '</li>');
 
 			}
-			console.log("accomplishment dates: " + accmpdate);
-			console.log("title: " + accmptitle);
-			console.log("description: " + accmpdes);*/
+			console.log(response.responseJSON.experience);
+		}//end of complete
+	}); //end of ajax
+});//end of document
+
+
+
+
+
+			/*
+
+			var skills=[];
+
+			$('.list').each(function(index, item) {
+				var element = $(item);
+
+				var category = element.find('dt').html();//find funciton can find ids/classes too
+				var level = element.find('p').html();
+				var descr = element.find('dd').html();
+
+				skills.push(category);
+				skills.push(level);
+				skills.push(descr);
+
+			});
+*/
+
+			//creating html for each occurance example
+			/*var favoriteDrinks = [];
+
+			$('.skill').each(function(index, item) {
+				var element = $(item);
+				var text = element.find('p').html();//find funciton can find ids/classes too
+				favoriteDrinks.push(text);
+
+			});
+
+			//accomplishments
+			var accmpdates = new Array;
+			var accmpdess = new Array;
+			var accmptitles = new Array;
+			var accmplishments = new Array;
+
+			for(i=0; i< response.responseJSON.accomplishments.length; i++)
+			{
+				accmpdates[i] = response.responseJSON.accomplishments[i].month_year;
+				accmpdess[i] = response.responseJSON.accomplishments[i].description;
+				accmptitles[i] = response.responseJSON.accomplishments[i].title;
+				accmplishments[i] = accmpdates[i] + " " + accmptitles[i] + ": " + accmpdess[i];
+			}
+
+			//console.log("accomplishment dates: " + accmpdates);
+			//console.log("title: " + accmptitles);
+			//console.log("description: " + accmpdess);
+			//console.log("accomplistments: " + accmplishments)
+	
+			//$('#tricks1').html(tricks[0]);
+			//$('#tricks2').html(tricks[1]);
+			//$('#tricks3').html(tricks[2]);
+			//$('#tricks4').html(tricks[3]);
+			//$('#tricks5').html(tricks[4]);
+
+			//$('#type1').html(type[0]);
+			//$('#type2').html(type[0]);
+			//$('#type3').html(type[0]);
+			//$('#type4').html(type[0]);
+			//$('#type5').html(type[0]);
+
+			//education
+
+			var begin = new Array;
+			var end = new Array;
+			var degrees = new Array;
+			var major = new Array;
+			var minor = new Array;
+			var gpa = new Array;
+			var name = new Array;
+			var duration = new Array;
+
+			for(i=0; i< response.responseJSON.schools.length; i++)
+			{
+				begin[i] = response.responseJSON.schools[i].end_month_year;
+				end[i] = response.responseJSON.schools[i].start_month_year;
+				gpa[i] = response.responseJSON.schools[i].gpa;
+				major[i] = response.responseJSON.schools[i].degree;major;
+				minor[i] = response.responseJSON.schools[i].minor;
+				name[i] = response.responseJSON.schools[i].name;
+				
+				duration[i] = begin[i] + " - " + end[i];
+			}
+
+			console.log(response.responseJSON);
 			
-			/*$('#tricks1').html(tricks[0]);
-			$('#tricks2').html(tricks[1]);
-			$('#tricks3').html(tricks[2]);
-			$('#tricks4').html(tricks[3]);
-			$('#tricks5').html(tricks[4]);
-
-			$('#type1').html(type[0]);
-			$('#type2').html(type[0]);
-			$('#type3').html(type[0]);
-			$('#type4').html(type[0]);
-			$('#type5').html(type[0]);
-
+/*
 
 		}
 	});
-});*/
-/*
-$(document).ready(function() {
-     console.log('im alive');
-     $.ajax("/api/resumes/51c20830a3672af4b3000001", {
-          complete : function(response){
-               console.log(response.responseJSON);
-
-
-          }
-     });
 });
-*/
 
-/*
+
+
+
  $(document).ready(function() {
      console.log('im alive');
      $.ajax("/api/resumes", {
@@ -116,74 +205,46 @@ $(document).ready(function() {
      });
 });
 */
+
+/*
 $(document).ready(function() {
 
-	//adds eduction block
-	$('.education_block_add').click(function() {
-		var html = $('.education_block').first().clone();
-		html.css('display' , 'none');
-		$(this).before(html);
-		html.slideDown(600);
-		return false;
-	});
 
-	//adds skills block
-	$('.skills_block_add').click(function() {
-		var html = $('.skills_block').first().clone();
-		html.css('display' , 'none');
-
-		$(this).before(html);
-		html.slideDown(600);
-		return false;
-	});
-
-	//adds accomplishments block
-	$('.accomplishments_block_add').click(function() {
-		var html = $('.accompl_block').first().clone();
-		html.css('display' , 'none');
-
-		$(this).before(html);
-		html.slideDown(600);
-		return false;
-	});
-	
-	//adds dropdown function
-	$('.dropdown-toggle').dropdown();
-
-	//populating an object and doing each to dynamically capture user input
-	//incomplete
-	$('#userDataForm').submit(function() {
-		//caching this - 
-		var userData = {};
-		userData.name = $('#fullname').val();
-		userData.name = $('#street_address').val();
-		userData.name = $('#fullname').val();
-
-		userData.schools = [];
-		var education_blocks = $('.education_block');
-
-		//machiko, carolyn, josh solution
-
-		school_groups
+//Capturing info notes from forms. 
+/*$(document).ready(function() {
+     console.log('im alive');
+     $.ajax("/api/resumes/51c20830a3672af4b3000001", {
+          complete : function(response){
+               console.log(response.responseJSON);
 
 
-
-
-
-		console.log(education_blocks);
-		console.log(userData);
-		return false; //type this in first; we know 
-		//it will because we are submitting with AJAX
-		//all code goes above 
-	});
-
-	
-
-
-
-	/*$(function() {
-    $( "#datepicker" ).datepicker();
-  });*/
-
-
+          }
+     });
 });
+
+$('#userDataForm').submit{function() {
+	var userData = {};
+	var userData.fullName = $('#name').val();
+	var name_array = fullName.split(' ');
+
+	userData.name_first = name_array[0];
+	userData.name_last = name_array[name_array.length - 1];
+	userData.schools = [];
+
+	var education_blocks = $('.education_block');
+	education_blocks.each(function(index, item) {
+		var startDate = $(item).find('.startDate').val();
+		var formattedDate = startDate.slice(5, 7) + startDate.slice(2, 4);
+		console.log(startDate);
+		userData.schools.push({
+			name : $(item).find('.name').val().
+			degree : $(item).find('.degree').val()
+			start_month_year: formattedDate
+		});
+	});
+	console.log(userData);
+	return false; 
+}
+	
+}*/
+
